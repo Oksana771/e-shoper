39cb77d0089ff163055978f5c415e4b360ce4333
Oksana <mikki07021994@gmail.com>
2020-11-27 12:09:26 +0200

    404 and notfound

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('contacts', views.contacts, name='contacts'),
-     path('*', views.not_found, name='not_found'),
+    path('not_found', views.not_found, name='not_found'),
 
 ]
